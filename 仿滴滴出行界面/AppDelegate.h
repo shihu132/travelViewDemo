a6c0957866fbbcb5273d42a5dba221318e40e684
shihu132 <13261089216@163.com>
2018-09-14 10:00:24 +0800

    Initial Commit

---
+++ 仿滴滴出行界面/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿滴滴出行界面
+//
+//  Created by joyshow on 2018/9/14.
+//  Copyright © 2018年 石虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿滴滴出行界面/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  仿滴滴出行界面
+//
+//  Created by joyshow on 2018/9/14.
+//  Copyright © 2018年 石虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
